5d9c64dd653d5cc43f2707b350161653457f2eb6
Mostech <123548702+safrinnetwork@users.noreply.github.com>
2024-08-10 22:55:05 +0800

    Update install_v7.sh

--- install_v7.sh
+++ install_v7.sh
@@ -28,9 +28,9 @@ CMD ["qemu-system-x86_64", "-m", "256M", "-smp", "1", "-hda", "/chr-7.15.3.qcow2
 EOF
 
 # Build Docker Image
-sudo docker build -t mikrotik-chr .
+sudo docker build -t mikrotik-chr-7 .
 
 # Jalankan Container MikroTik CHR
-sudo docker run --name mikrotik-chr --restart unless-stopped -p 8291:8291 -p 8728:8728 -p 8729:8729 -p 2222:22 -p 80:80 -p 443:443 -p 21:21 mikrotik-chr
+sudo docker run --name mikrotik-chr-7 --restart unless-stopped -p 8291:8291 -p 8728:8728 -p 8729:8729 -p 2222:22 -p 80:80 -p 443:443 -p 21:21 mikrotik-chr-7
 
-echo "MikroTik CHR telah berhasil diinstal dan dijalankan dalam Docker."
+echo "MikroTik CHR telah berhasil diinstal dan dijalankan dalam Docker dengan nama mikrotik-chr-7."
